664cdf505c7132b8224c7bcae366ad8c4282e7c3
indra-182 <mahadiindra2@gmail.com>
2023-01-09 13:09:12 +0700

    add feature automatic close form when successfully submit data

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
+  final Function addTransaction;
+  NewTransaction(this.addTransaction);
+
+  @override
+  State<NewTransaction> createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
-  final Function addTransaction;
 
-  NewTransaction(this.addTransaction);
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    widget.addTransaction(
+      enteredTitle,
+      enteredAmount,
+    );
+
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,16 +41,17 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
               controller: titleController,
+              onSubmitted: (_) => submitData(),
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
               keyboardType: TextInputType.number,
               inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              onSubmitted: (_) => submitData(),
             ),
             FlatButton(
-              onPressed: () => addTransaction(
-                  titleController.text, num.parse(amountController.text)),
+              onPressed: submitData,
               child: Text('Add Transaction'),
               textColor: Colors.orange,
             ),
